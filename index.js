9aa81375cb2f300bae73a473cf05b26579d62afd
Steven Luzuriaga <75744001+steven-altitude@users.noreply.github.com>
2023-03-31 18:06:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const PORT = 3000;
   
 app.listen(PORT, (error) =>{
     if(!error)
-        console.log("Server is Successfully Running, 
-                   and App is listening on port "+ PORT)
+        console.log("Server is Successfully Running "+ PORT)
     else 
         console.log("Error occurred, server can't start", error);
     },
