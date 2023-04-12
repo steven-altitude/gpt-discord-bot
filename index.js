1268395d50f6eea8b114e630420efba751f0cbde
Steven Luzuriaga <75744001+steven-altitude@users.noreply.github.com>
2023-04-11 21:05:32 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,79 +1,45 @@
-require('dotenv/config');
-const { Client, IntentsBitField } = require('discord.js');
+require('dotenv').config();
+const { Client, Intents } = require('discord.js');
 const { Configuration, OpenAIApi } = require('openai');
-const express = require('express');
-  
-const app = express();
-const PORT = 3000;
-  
-app.listen(PORT, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running,and App is listening on port "+ PORT)
-    else 
-        console.log("Error occurred, server can't start", error);
-    });
-
 
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-    ],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-
 client.on('ready', () => {
-    console.log('The bot is online!');
+  console.log(`Logged in as ${client.user.tag}`);
 });
 
 const configuration = new Configuration({
-    apiKey: process.env.API_KEY,
+  apiKey: process.env.API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 client.on('messageCreate', async (message) => {
-    if (message.author.bot) return;
-    if (message.channel.id !== process.env.CHANNEL_ID) return;
-    if (message.content.startsWith('!')) return;
+  if (message.author.bot) return;
+  if (message.channel.id !== process.env.CHANNEL_ID) return;
+  if (message.content.startsWith('!')) return;
 
-    let conversationLog = [{ role: 'system', content: 'You are a sarcastic chatbot.' }];
+  const conversationLog = [
+    {
+      role: 'user',
+      text: message.content,
+    },
+  ];
 
-    try {
-        await message.channel.sendTyping();
+  try {
+    const result = await openai.createCompletion({
+      engine: 'davinci-codex',
+      prompt: conversationLog,
+      maxTokens: 100,
+    });
 
-        let prevMessages = await message.channel.messages.fetch({ limit: 15 });
-        prevMessages.reverse();
-
-        prevMessages.forEach((msg) => {
-            if (message.content.startsWith('!')) return;
-            if (msg.author.id !== client.user.id && message.author.bot) return;
-            if (msg.author.id !== message.author.id) return;
-
-            conversationLog.push({
-                role: 'user',
-                content: msg.content,
-            });
-        });
-
-        const result = await openai
-            .createChatCompletion({
-                model: 'gpt-3.5-turbo',
-                messages: conversationLog,
-                // max_tokens: 256, // limit token usage
-            })
-            .catch((error) => {
-                console.log(`OPENAI ERR: ${error}`);
-            });
-
-        message.reply(result.data.choices[0].message);
-    } catch (error) {
-        console.log(`ERR: ${error}`);
-    }
+    const response = result.choices[0].text;
+    message.reply(response);
+  } catch (error) {
+    console.error(error);
+    message.reply('An error occurred while processing your request.');
+  }
 });
 
 client.login(process.env.TOKEN);
-
-
-// Export the Express API
-module.exports = app;
